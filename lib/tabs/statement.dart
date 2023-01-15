65ed68fd300f1b5301d33ee9527c593364627fa2
asjklj <119600073+asjklj@users.noreply.github.com>
2023-01-15 11:42:18 +0800

    Update statement.dart
    statement

--- lib/tabs/statement.dart
+++ lib/tabs/statement.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:io'; //导IO包
+import 'dart:convert';
+
+import 'package:flutter/scheduler.dart'; //解码和编码JSON
+
+List SelectedValue = [1,2,1,2,24,1];
+List conditioner=["pref","mode","speed","temp","onoff"];
+bool flag = true;
 
 class Statement extends StatefulWidget {
   const Statement({super.key});
@@ -8,10 +16,12 @@ class Statement extends StatefulWidget {
 }
 
 class _StatementState extends State<Statement> {
-  var _selectedValue = 1;
-  var _selectedIndex = 0;
-
   @override
+  void initState() {
+    // TODO: implement initState
+    GetStatement();  
+    setState(() {});
+  }
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
@@ -23,16 +33,18 @@ class _StatementState extends State<Statement> {
         Container(
             height: 50,
             width: 50,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
-              gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 223, 240, 253),
+              border: Border.all(color: Colors.blue),
+              image: DecorationImage(image: NetworkImage(""),fit: BoxFit.cover),
+              // boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
+              // gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
             ),
             child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               const Text("偏好 :"),
               DropdownButton(
-                value: _selectedValue,
+                value: SelectedValue[1],
                 items: const [
                   DropdownMenuItem(
                     value: 1,
@@ -46,26 +58,35 @@ class _StatementState extends State<Statement> {
                     value: 3,
                     child: Text("3"),
                   ),
+                  DropdownMenuItem(
+                    value: 4,
+                    child: Text("4"),
+                  ),
                 ],
                 onChanged: (value) {
-                  _selectedValue = value!;
-                  setState(() {});
+                  SelectedValue[1] = value!;
+                  setState(() {
+                    PostStatement(1);
+                    GetStatement();
+                  });
                 },
               )
             ])),
         Container(
             height: 50,
             width: 50,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
-              gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 223, 240, 253),
+              image: DecorationImage(image: NetworkImage(""),fit: BoxFit.cover),
+              border: Border.all(color: Colors.blue),
+              // boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
+              // gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
             ),
             child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               const Text("模式 :"),
               DropdownButton(
-                value: _selectedValue,
+                value: SelectedValue[2],
                 items: const [
                   DropdownMenuItem(
                     value: 1,
@@ -81,24 +102,29 @@ class _StatementState extends State<Statement> {
                   ),
                 ],
                 onChanged: (value) {
-                  _selectedValue = value!;
-                  setState(() {});
+                  SelectedValue[2] = value!;
+                  setState(() {
+                    PostStatement(2);
+                    GetStatement();
+                  });
                 },
               )
             ])),
         Container(
             height: 50,
             width: 50,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
-              gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 223, 240, 253),
+              border: Border.all(color: Colors.blue),
+              image: DecorationImage(image: NetworkImage("https://www.iconfinder.com/icons/9025042/wind_weather_icon"),fit: BoxFit.cover),
+              // boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
+              // gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
             ),
             child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               const Text("风速 :"),
               DropdownButton(
-                value: _selectedValue,
+                value: SelectedValue[3],
                 items: const [
                   DropdownMenuItem(
                     value: 1,
@@ -114,48 +140,140 @@ class _StatementState extends State<Statement> {
                   ),
                 ],
                 onChanged: (value) {
-                  _selectedValue = value!;
-                  setState(() {});
+                  SelectedValue[3] = value!;
+                  setState(() {
+                     PostStatement(3);
+                     GetStatement();
+                     
+                  });
                 },
               )
             ])),
         Container(
             height: 50,
             width: 50,
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
-              gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 223, 240, 253),
+              image: DecorationImage(image: NetworkImage(""),fit: BoxFit.cover),
+              border: Border.all(color: Colors.blue),
+              // boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 20.0)],
+              // gradient: LinearGradient(colors: [Colors.blue, Colors.white]),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
             ),
             child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               const Text("温度 :"),
               DropdownButton(
-                value: _selectedValue,
+                value: SelectedValue[4],
                 items: const [
                   DropdownMenuItem(
-                    value: 1,
+                    value: 20,
+                    child: Text("20"),
+                  ),
+                  DropdownMenuItem(
+                    value: 21,
+                    child: Text("21"),
+                  ),
+                  DropdownMenuItem(
+                    value: 22,
+                    child: Text("22"),
+                  ),
+                  DropdownMenuItem(
+                    value: 23,
+                    child: Text("23"),
+                  ),
+                  DropdownMenuItem(
+                    value: 24,
                     child: Text("24"),
                   ),
                   DropdownMenuItem(
-                    value: 2,
-                    child: Text("2"),
+                    value: 25,
+                    child: Text("25"),
                   ),
                   DropdownMenuItem(
-                    value: 3,
-                    child: Text("3"),
+                    value: 26,
+                    child: Text("26"),
                   ),
                 ],
                 onChanged: (value) {
-                  _selectedValue = value!;
-                  setState(() {});
+                  SelectedValue[4] = value!;
+                  setState(() {
+                    PostStatement(4);
+                    GetStatement();
+                  });
                 },
               )
-            ])
+            ])),
+        Container(
+          alignment: Alignment.centerRight,
+          child: const Text(
+            "开关:",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
             ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          child: Transform.scale(
+            scale: 2.0,
+            child: Switch(
+                value: flag,
+                onChanged: ((value) {
+                  setState(() {
+                    if(value==false){flag=false;SelectedValue[5]=0;}
+                    else {flag=true;SelectedValue[5]=1;}                    
+                    PostStatement(5);
+                    GetStatement();
+                  });
+                })),
+          ),
+        )
       ],
     );
   }
 }
-void GetStatement(index) {}
-void PostStatement(index) {}
+
+void GetStatement() async {
+  String responseBody;
+  //1.创建HttpClient
+  var httpClient = new HttpClient();
+  //2.构造Uri
+  var request = await httpClient
+      .getUrl(Uri.parse("http://81.68.216.118:9090/api/get/airset/"));
+  //3.关闭请求，等待响应
+  var response = await request.close();
+  //4.进行解码，获取数据
+  if (response.statusCode == 200) {
+    //拿到请求的数据
+    responseBody = await response.transform(utf8.decoder).join();
+    //先不解析打印数据
+    Map<String, dynamic> map = json.decode(responseBody);
+    SelectedValue[1] = map['pref'];
+    SelectedValue[2] = map['mode'];
+    SelectedValue[3] = map['speed'];
+    SelectedValue[4] = map['temp'];
+    SelectedValue[5]= map['on_off'];
+    if (SelectedValue[5]==0) {flag=false;}
+    else {flag=true;}
+  }
+}
+
+void PostStatement(index) async {
+  var httpClient = new HttpClient();
+  int n=SelectedValue[index];
+  String m=conditioner[index-1];
+  String s = "$m?num=$n";
+  // var uri = Uri(
+  //     scheme: 'http',
+  //     host: '81.68.216.118',
+  //     path: ':9090/api/post/airset/temp',
+  //     queryParameters: {'temp':26});
+  // var uri2=Uri.https('www.wanandroid.com', '/user/login',{'username': 'wanandroidUser1', 'password': '123456'});
+  HttpClientRequest request = await httpClient
+      .postUrl(Uri.parse("http://81.68.216.118:9090/api/post/airset/$s"));
+  HttpClientResponse response = await request.close();
+  if (response.statusCode == 200) {
+    String responseBody = await response.transform(utf8.decoder).join();
+  }
+}
