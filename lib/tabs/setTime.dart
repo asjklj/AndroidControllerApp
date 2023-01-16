ad7cc0ce04c0ff88b5eeb4691ead6d1521708461
叶落秋波 <116635359+Wbfyr@users.noreply.github.com>
2023-01-16 18:51:57 +0800

    Update setTime.dart

--- lib/tabs/setTime.dart
+++ lib/tabs/setTime.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class SetTime extends StatefulWidget {
   const SetTime({super.key});
 
@@ -7,18 +8,37 @@ class SetTime extends StatefulWidget {
 }
 
 class _SetTimeState extends State<SetTime> {
+  // List<Widget> _setListData() {
+  //   List<Widget> list = [];
+  //   for (var i = 1; i <= 7; i++) {
+  //     list.add(ListTile(
+  //       title: Text("day$i"),
+  //     ));
+  //   }
+  //   return ;
+  // }
+  List weekday = [
+    "Monday",
+    "Tuesday",
+    "Wenesday",
+    "Thursday",
+    "Friday",
+    "Saterday",
+    "Sunday"
+  ];
 
-List<Widget>_setListData(){ 
-  List<Widget> list=[];
-  for(var i=1;i<=7;i++){
-    list.add(ListTile(title:Text("day$i"),));
-    }
-    return list;
-}
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: _setListData()
-    );
+    return ListView.builder(
+        itemCount: 7,
+        itemBuilder: (context, i) {
+          return ListTile(
+            title: Text(weekday[i]),
+            trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, weekday[i]); // do something
+            },
+          );
+        });
   }
 }
