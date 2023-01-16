35c855d3e7d9f4132511451ab725c8e414a0d5d1
asjklj <119600073+asjklj@users.noreply.github.com>
2023-01-16 22:35:35 +0800

    Update home.dart

--- lib/tabs/home.dart
+++ lib/tabs/home.dart
@@ -9,6 +9,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("首页"),);
+    return Center(child:Column(children: [Image(image:AssetImage('images/gongdao.jpg') ,fit:BoxFit.cover ,),Text(
+  """使用指南:
+1.点击“状态”可以查看并设置当前空调设置
+(偏好3为正常且数字越小空调自动设置越冷;
+风速数值越大越高)
+2.点击“定时”可以设置需要空调开启的时间""",style:TextStyle(color: Color.fromARGB(255, 45, 123, 186)))]) ,) ;
   }
 }
