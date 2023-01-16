8c536c05746111069baf67470227d13c2d1900b9
asjklj <119600073+asjklj@users.noreply.github.com>
2023-01-16 22:33:09 +0800

    Update tabs.dart

--- lib/tabs/tabs.dart
+++ lib/tabs/tabs.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import './statement.dart';
 import './home.dart';
 import './setTime.dart';
-class Tabs extends StatefulWidget {//底部导航栏选择
+
+class Tabs extends StatefulWidget {
+  //底部导航栏选择
   const Tabs({super.key});
 
   @override
@@ -10,28 +12,27 @@ class Tabs extends StatefulWidget {//底部导航栏选择
 }
 
 class _TabsState extends State<Tabs> {
-  int _currentIndex=0;
-  final List<Widget>_pages=const [HomePage(),Statement(),SetTime()];
-    @override
+  int _currentIndex = 0;
+  final List<Widget> _pages = const [HomePage(), Statement(), SetTime()];
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title:  const Text("桌上智能辅助空调控制盒app")),
+    return Scaffold(
+      appBar: AppBar(title: const Text("桌上智能辅助空调控制盒移动端",style: TextStyle(color: Color.fromARGB(255, 95, 140, 217),fontSize: 20),textAlign: TextAlign.center,)),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap:(index){
-setState((){
-  _currentIndex=index;
-});
-        },
-        items:const [
-        BottomNavigationBarItem(icon: Icon(Icons.home),
-        label: "首页"),
-        BottomNavigationBarItem(icon: Icon(Icons.display_settings),
-        label: "状态"),
-        BottomNavigationBarItem(icon: Icon(Icons.add_alarm),
-        label: "定时")
-      ]),
-      );
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          selectedItemColor: Color.fromARGB(255, 98, 166, 222),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.display_settings), label: "状态"),
+            BottomNavigationBarItem(icon: Icon(Icons.add_alarm), label: "定时")
+          ]),
+    );
   }
 }
-
